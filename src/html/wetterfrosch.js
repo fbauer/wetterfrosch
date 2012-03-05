29aa734e2df337ba87360539ccff271c80623852
Florian Bauer <flo@boeserwolf>
2012-03-05 08:37:01 +0100

    used fixjsstyle

--- src/html/wetterfrosch.js
+++ src/html/wetterfrosch.js
@@ -4,41 +4,41 @@
 ajaxDataRenderer = function(url, plot, options) {
     var ret = null;
     $.ajax({
-	// have to use synchronous here, else the function 
+	// have to use synchronous here, else the function
 	// will return before the data is fetched
 	async: false,
 	url: url,
-	dataType:"json",
+	dataType: 'json',
 	success: function(data) {
 	    ret = extractTemp(data);
 	}
     });
     return ret;
 };
- 
+
 var extractTemp = function(data) {
     var accu = {};
     var channels = [];
     jQuery.each(data,
 		function(i, n) {
-		    var chan = n["ch"];
-		    if(!(chan in accu)) {
+		    var chan = n['ch'];
+		    if (!(chan in accu)) {
 			accu[chan] = [];
 			channels.push(chan);
-		    };
-		    accu[chan].push([new Date(n["ts"]), n["temp"]]);
-		})
+		    }
+		    accu[chan].push([new Date(n['ts']), n['temp']]);
+		});
     var result = [];
     var max_chan = Math.max.apply(Math, channels);
-    for(var i=1; i<=max_chan; i++) {
-	if(i in accu) {
+    for (var i = 1; i <= max_chan; i++) {
+	if (i in accu) {
 	    result.push(accu[i]);
 	}
 	else {
 	    result.push([]);
-	};
-    };
-    return(result);
+	}
+    }
+    return (result);
 };
 
 
