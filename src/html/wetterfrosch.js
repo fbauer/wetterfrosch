b3af3acb61200d5f66865307f2774ce4c69f30f4
Florian Bauer <flo@boeserwolf>
2012-03-05 08:48:27 +0100

    some tabs crept in, removed them

--- src/html/wetterfrosch.js
+++ src/html/wetterfrosch.js
@@ -1,17 +1,17 @@
-   // Our ajax data renderer which here retrieves a text file.
-  // it could contact any source and pull data, however.
-  // The options argument isn't used in this renderer.
+// Our ajax data renderer which here retrieves a text file.
+// it could contact any source and pull data, however.
+// The options argument isn't used in this renderer.
 ajaxDataRenderer = function(url, plot, options) {
     var ret = null;
     $.ajax({
-	// have to use synchronous here, else the function
-	// will return before the data is fetched
-	async: false,
-	url: url,
-	dataType: 'json',
-	success: function(data) {
-	    ret = extractTemp(data);
-	}
+    // have to use synchronous here, else the function
+    // will return before the data is fetched
+    async: false,
+    url: url,
+    dataType: 'json',
+    success: function(data) {
+        ret = extractTemp(data);
+    }
     });
     return ret;
 };
@@ -20,23 +20,23 @@ var extractTemp = function(data) {
     var accu = {};
     var channels = [];
     jQuery.each(data,
-		function(i, n) {
-		    var chan = n['ch'];
-		    if (!(chan in accu)) {
-			accu[chan] = [];
-			channels.push(chan);
-		    }
-		    accu[chan].push([new Date(n['ts']), n['temp']]);
-		});
+        function(i, n) {
+            var chan = n['ch'];
+            if (!(chan in accu)) {
+            accu[chan] = [];
+            channels.push(chan);
+            }
+            accu[chan].push([new Date(n['ts']), n['temp']]);
+        });
     var result = [];
     var max_chan = Math.max.apply(Math, channels);
     for (var i = 1; i <= max_chan; i++) {
-	if (i in accu) {
-	    result.push(accu[i]);
-	}
-	else {
-	    result.push([]);
-	}
+    if (i in accu) {
+        result.push(accu[i]);
+    }
+    else {
+        result.push([]);
+    }
     }
     return (result);
 };
