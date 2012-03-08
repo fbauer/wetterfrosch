f048fa3c300c2546626f43a47bebf4f7c43e0b92
Florian Bauer <flo@boeserwolf>
2012-03-08 07:18:13 +0100

    reindented

--- test/test.js
+++ test/test.js
@@ -10,57 +10,62 @@ var ts='ts';
 
 test("extractTemp", function() {
     var input = [];
-    deepEqual(extractTemp(input), [], 
+    deepEqual(extractTemp(input), [],
 	      "Empty lists should be handled correctly");
     input = [{ch: 1, temp: 2.0, ts: new Date("2012-09-01")},
 	     {ch: 1, temp: 3.0, ts: new Date("2012-09-02")}
 	    ];
-    var output = [[[new Date("2012-09-01"), 2.0], [new Date("2012-09-02"), 3.0]]
-		 ];
-    deepEqual(extractTemp(input), output, 
+    var output = [[[new Date("2012-09-01"), 2.0],
+                   [new Date("2012-09-02"), 3.0]]];
+    deepEqual(extractTemp(input), output,
 	      "Data for single plot is aggregated in first sub-array");
- 
+
     input = [{ch: 1, temp: 2.0, ts: "2012-09-01"},
 	     {ch: 1, temp: 3.0, ts: "2012-09-02"}
 	    ];
-    deepEqual(extractTemp(input), output, 
+    deepEqual(extractTemp(input), output,
 	      "Time stamps given as string are converted to Date");
 
     var input2ch = [{ch: 1, temp: 2.0, ts: "2012-09-01"},
 		    {ch: 2, temp: 3.0, ts: "2012-09-02"},
 		    {ch: 1, temp: 3.0, ts: "2012-09-02"}
 		   ];
-    var output2ch = [[[new Date("2012-09-01"), 2.0], [new Date("2012-09-02"), 3.0]],
-		     [[new Date("2012-09-02"), 3.0]]
-		    ];
-    deepEqual(extractTemp(input2ch), output2ch, 
+    var output2ch = [[[new Date("2012-09-01"), 2.0],
+                      [new Date("2012-09-02"), 3.0]],
+		     [[new Date("2012-09-02"), 3.0]]];
+    deepEqual(extractTemp(input2ch), output2ch,
 	      "Input objects are sorted according to their channel ch");
 
     var input2ch_sparse = [{ch: 1, temp: 2.0, ts: "2012-09-01"},
 			   {ch: 3, temp: 3.0, ts: "2012-09-02"},
 			   {ch: 1, temp: 3.0, ts: "2012-09-02"}
 			  ];
-    var output2ch_sparse = [[[new Date("2012-09-01"), 2.0], [new Date("2012-09-02"), 3.0]],
+    var output2ch_sparse = [[[new Date("2012-09-01"), 2.0],
+                             [new Date("2012-09-02"), 3.0]],
 			    [],
-			    [[new Date("2012-09-02"), 3.0]]
-			   ];
-    deepEqual(extractTemp(input2ch_sparse), output2ch_sparse, 
+			    [[new Date("2012-09-02"), 3.0]]];
+    deepEqual(extractTemp(input2ch_sparse), output2ch_sparse,
 	      "Missing channels are filled with an empty array");
     input = [{ch: 1, temp: 3.0, ts: "2012 09 02"}];
 
     raises(extractTemp(input), "a",
 	   "raise Exception in case of malformed date format");
 });
-toISOTimestamp = function(date) {return(MochiKit.DateTime.toISOTimestamp(date, true))};
+
+var toISOTimestamp = function(date) {
+                         return(MochiKit.DateTime.toISOTimestamp(date, true));};
+
 test("RFC 3339", function() {
-    datestring = "2012-03-04T12:04:59Z";
+    var datestring = "2012-03-04T12:04:59Z";
     var input = MochiKit.DateTime.isoTimestamp(datestring);
     equal(toISOTimestamp(input), "2012-03-04T12:04:59Z", "sanity check");
     var my_jsdate = new $.jsDate(input);
-    equal(toISOTimestamp(my_jsdate), "2012-03-04T12:04:59Z",
+    equal(toISOTimestamp(my_jsdate),
+          "2012-03-04T12:04:59Z",
 	  "calling jsDate with a Date instance works" );
     my_jsdate = new $.jsDate(datestring);
-    equal(my_jsdate.toString(), "NaN", 
+    equal(my_jsdate.toString(),
+          "NaN",
 	  "calling jsDate with an RFC 3339 timestamp results in a NaN" );
 });
 
