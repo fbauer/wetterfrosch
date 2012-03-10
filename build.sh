57066a29c76cef472f4ea02c5b20f8c03af87f3f
Florian Bauer <flo@boeserwolf>
2012-03-10 12:59:17 +0100

    concatenate all javscript together during build
    This will allow minification later on.

--- build.sh
+++ build.sh
@@ -13,3 +13,11 @@ cp "src/jqplot/plugins/jqplot.canvasTextRenderer.min.js" "$BUILDDIR"
 cp "src/jqplot/plugins/jqplot.canvasAxisLabelRenderer.min.js" "$BUILDDIR"
 cp src/html/* "$BUILDDIR"
 python src/datagen.py > "$BUILDDIR"/jsondata.txt
+
+cat src/jqplot/jquery.min.js > wetterfrosch_min.js
+cat src/jqplot/jquery.jqplot.min.js >> wetterfrosch_min.js
+cat src/jqplot/plugins/jqplot.dateAxisRenderer.min.js >> wetterfrosch_min.js
+cat src/jqplot/plugins/jqplot.canvasTextRenderer.min.js >> wetterfrosch_min.js
+cat src/jqplot/plugins/jqplot.canvasAxisLabelRenderer.min.js >> "$BUILDDIR"/wetterfrosch.min.js
+cat src/html/MochiKit.js >> "$BUILDDIR"/wetterfrosch.min.js
+cat src/html/wetterfrosch.js >> "$BUILDDIR"/wetterfrosch.min.js
