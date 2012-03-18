3aeceabb77dd43d83dbf9d2528f3e4911f9c0727
Florian Bauer <bauflo3@web.de>
2012-03-18 20:56:54 +0100

    Add copyright

--- src/datagen.py
+++ src/datagen.py
@@ -1,6 +1,8 @@
 ##############################################################################
 #
 # This file is part of wetterfrosch.
+# 
+# Copyright 2012 Florian Bauer
 #
 # wetterfrosch is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
