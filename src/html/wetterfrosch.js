cfa0ca77e953fdf14887ff0c5f5c39523b2866d6
Florian Bauer <bauflo3@web.de>
2012-03-18 20:57:30 +0100

    Add copyright

--- src/html/wetterfrosch.js
+++ src/html/wetterfrosch.js
@@ -2,6 +2,9 @@
 //
 // This file is part of wetterfrosch.
 //
+
+// Copyright 2012 Florian Bauer
+//
 // wetterfrosch is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
